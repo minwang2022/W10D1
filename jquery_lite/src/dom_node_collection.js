7070ba61acd82ddc167ce16ed587d328a57da414
min <minwang.bus@gmail.com>
2021-04-26 14:26:10 -0400

    workong on append mathod

--- jquery_lite/src/dom_node_collection.js
+++ jquery_lite/src/dom_node_collection.js
@@ -9,12 +9,37 @@ class DOMNodeCollection {
             return this.array[0].innerHTML;      
         } else {
             this.array.forEach((ele) => {
-                ele.innerHTML = str; //undefined
+                ele.innerHTML = str;
             })
         }     
     }
 
-    
+    empty() {
+        this.array.forEach(el => {
+            el.innerHTML = "";
+        })
+    }
+
+    append(arg) {       
+        if (typeof arg === "string") {
+            this.array.forEach((ele) => {
+                ele.innerHTML += arg;
+            })
+        } else if (arg instanceof HTMLElement) {
+            this.array.forEach((ele) => {
+                ele.innerHTML += $l(arg);
+            })
+        } else if (arg instanceof DOMNodeCollection){
+            this.array.forEach((ele) => {
+                arg.forEach(node => {
+                    ele.appendChild(node);
+                })
+            })
+        }
+    }
+
+  
+
 }
 
 
